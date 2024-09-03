6d05049c08ceb25f19744efb4659f1a05802ccea
amanda-santos <amandasf0200@gmail.com>
2024-09-03 19:39:47 -0300

    feat: create app header

---
+++ apps/web/src/app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import { isAuthenticated } from '@/helpers/is-authenticated'
+import { redirect } from 'next/navigation'
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  if (!isAuthenticated()) {
+    redirect('/auth/sign-in')
+  }
+
+  return children
+}
