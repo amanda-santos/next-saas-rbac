7b316247c1967f8f58773677163e50a495cd74bc
amanda-santos <amandasf0200@gmail.com>
2024-09-07 23:10:19 -0300

    chore: setup env variables in web app

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,15 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGitHub() {
   const gitHubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
 
-  gitHubSignInURL.searchParams.set('client_id', 'my-client-id')
+  gitHubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   gitHubSignInURL.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   gitHubSignInURL.searchParams.set('scope', 'user')
 
