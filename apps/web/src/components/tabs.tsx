344dc4dddd21dd24320e2ed34029187dcf3f38d3
amanda-santos <amandasf0200@gmail.com>
2024-09-14 19:57:51 -0300

    feat: add tabs navigation in organization pages

---
+++ apps/web/src/components/tabs.tsx
@@ -0,0 +1,43 @@
+import { getCurrentOrganizationSlug } from '@/auth'
+
+import { NavLink } from './nav-link'
+import { Button } from './ui/button'
+
+export function Tabs() {
+  const currentOrganizationSlug = getCurrentOrganizationSlug()
+
+  return (
+    <div className="border-b py-4">
+      <nav className="mx-auto flex max-w-[1200px] items-center gap-2">
+        <Button
+          asChild
+          variant="ghost"
+          size="sm"
+          className="border border-transparent text-muted-foreground data-[current=true]:border-border data-[current=true]:text-foreground"
+        >
+          <NavLink href={`/org/${currentOrganizationSlug}`}>Projects</NavLink>
+        </Button>
+        <Button
+          asChild
+          variant="ghost"
+          size="sm"
+          className="border border-transparent text-muted-foreground data-[current=true]:border-border data-[current=true]:text-foreground"
+        >
+          <NavLink href={`/org/${currentOrganizationSlug}/members`}>
+            Members
+          </NavLink>
+        </Button>
+        <Button
+          asChild
+          variant="ghost"
+          size="sm"
+          className="border border-transparent text-muted-foreground data-[current=true]:border-border data-[current=true]:text-foreground"
+        >
+          <NavLink href={`/org/${currentOrganizationSlug}/settings`}>
+            Settings & Billing
+          </NavLink>
+        </Button>
+      </nav>
+    </div>
+  )
+}
